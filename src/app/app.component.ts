f6686ebbdf8e2a7239fcdb55274852d096df94a1
Dannie-25 <drake.meav@gmail.com>
2024-03-01 20:03:05 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ruleta-odma';
+  casillas: number[] = Array.from({ length: 36 }, (_, i) => i + 1);
+  numeroApostado: number | undefined;
+  numeroCanica: number | undefined;
+  resultado: string | undefined;
+
+  mezclarNumeros(): void {
+    this.casillas.sort(() => Math.random() - 0.5);
+  }
+
+  lanzarCanica(): void {
+    this.numeroCanica = Math.floor(Math.random() * this.casillas.length) + 1;
+    if (this.numeroApostado === this.numeroCanica) {
+      this.resultado = '¡Ganaste!';
+    } else {
+      this.resultado = 'Perdiste :v';
+    }
+  }
 }
